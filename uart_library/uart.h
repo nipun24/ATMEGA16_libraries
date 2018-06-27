4c52d2f7b549e9ae0ac8dd81515fe80c7c0d3c68
Nipun Haldar <nipunhaldar24@gmail.com>
2018-06-27 22:02:36 +0530

    Addded uart_library
    -Added UART Library
    -Added uart_README.md
    -Added UART schematic

---
+++ uart_library/uart.h
@@ -0,0 +1,26 @@
+void uart_setup()
+{
+	UBRRL = UBRR_VALUE;
+	UBRRH = (UBRR_VALUE>>8);
+    /*
+    TXEN = 1 and RXEN = 1, enabling UART
+    */
+	UCSRB = (1<<TXEN)|(1<<RXEN);
+    /*
+    1. URSEL = 1, data will be stored in UBRRL first. Low-->High storing.
+    2. UCSZ1 = 1 and UCSZ0 = 1, for 8-bit data as atmega16 is a 8 bit processor.
+    */
+	UCSRC = (1<<URSEL)|(1<<UCSZ1)|(1<<UCSZ0);
+}
+
+void uart_send(unsigned char data)
+{
+	while(!(UCSRA & (1<<UDRE)));
+	UDR = data;
+}
+
+unsigned char uart_rec()
+{
+	while(!(UCSRA & (1<<RXC)));
+	return UDR;
+}
